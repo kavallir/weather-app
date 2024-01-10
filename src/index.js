265ca4b5eaad884795499b9ed14ecf4da63d6b49
kavallir <152403875+kavallir@users.noreply.github.com>
2024-01-10 17:34:37 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 function refreshWeather (response) {
     let tempElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
+    let descriptionElement = document.querySelector("#description");
     let cityElement = document.querySelector("#city");
-    // let emojiElement = document.querySelector("#emoji")
+    let windElement = document.querySelector("#wind-speed");
+    let humidityElement = document.querySelector("#humidity");
+    let timeElement = document.querySelector("#time");
+    let date = new Date (response.data.time * 1000);
+    let temperature = response.data.temperature.current;
+    descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city;
-    // emojiElement.innerHTML = response.data.icon;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     tempElement.innerHTML = Math.round(temperature);
-    
+    timeElement.innerHTML = formatDate(date);
 }
 
-
+function formatDate (date) {
+    
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+        `Sunday`, 
+        `Monday`, 
+        `Tuesday`, 
+        `Wednesday`, 
+        `Thursday`, 
+        `Friday`, 
+        `Saturday`
+    ];
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity (city) {
 let apiKey = "fea658ob71c06a80c3410a8at40a9b7a";
