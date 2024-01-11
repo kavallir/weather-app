95ffb33d86bec98a0ce2cef6cfd6cace20514373
kavallir <152403875+kavallir@users.noreply.github.com>
2024-01-11 15:15:40 +0100

    Injected the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -53,9 +53,37 @@ function handleSearchSubmit (event) {
     searchCity(searchInput.value);
 }
 
-
+function displayForecast () {
+    let days = [
+        `Sun`, 
+        `Mon`, 
+        `Tue`, 
+        `Wed`, 
+        `Thu`, 
+        // `Fri`, 
+        // `Sat`
+    ]; 
+    let forecastHtml = "";
+    days.forEach(function (day) {
+        forecastHtml = forecastHtml + `<div class="weather-forecast-day">
+        <div class="col-2">
+             <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon"><img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
+            </div>
+            <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">18</span>/
+                <span class="weather-forecast-temperature-min">12</span></div> 
+            </div>
+        </div>`
+    });
+    let forecastElement = document.querySelector("#forecast")
+    forecastElement.innerHTML = forecastHtml;
+    };
+    
 
 let searchFormelement = document.querySelector("#search-form");
 searchFormelement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Kyiv");
+
+displayForecast();
